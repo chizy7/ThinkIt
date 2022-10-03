1fe2ff1b5541f64d2d65f3f8770003181222ecc5
Chizaram Chibueze <chizy7@gmail.com>
2022-10-02 21:25:04 -0400

    Adding firebase to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:thinkit/Pages/HomePage.dart';
 
-void main() => runApp(thinkit());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(thinkit());
+}
 
 class thinkit extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +17,20 @@ class thinkit extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: HomePage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return HomePage(
+              title: 'Flutter Demo',
+            );
+          }
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
