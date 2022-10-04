08886886a0329896058c7cecd5ee4666712f6252
cherorafi <saidun2001@gmail.com>
2022-10-04 03:13:49 -0400

    Most functionality of results page working, needs percentages from questions page

--- lib/Pages/ResultsPage.dart
+++ lib/Pages/ResultsPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'question_implementation.dart';
 
 class ResultsPage extends StatefulWidget {
   const ResultsPage({super.key});
@@ -9,6 +11,14 @@ class ResultsPage extends StatefulWidget {
 
 class ResultsPageState extends State<ResultsPage> {
   String currentResultImage = 'images/IdealistResult.png';
+
+  // Needs percentages
+  int idealPercentage = 0;
+  int synPercentage = 0;
+  int pragPercentage = 0;
+  int realPercentage = 0;
+  int analystPercentage = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,73 +27,107 @@ class ResultsPageState extends State<ResultsPage> {
             image: DecorationImage(
                 image: AssetImage('assets/images/thinkit_bg.png'),
                 fit: BoxFit.cover)),
+        
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            Padding(padding: const EdgeInsets.fromLTRB(40.0, 0, 0, 0),
+              child: Text("These are the results, you are:\n\n\n${idealPercentage.toString()}% Idealist\n\n\n${synPercentage.toString()}% Synthesist\n\n\n${pragPercentage.toString()}% Pragmatist\n\n\n${analystPercentage.toString()}% Analyst\n\n\n${realPercentage.toString()}% Realist", 
+              style: GoogleFonts.architectsDaughter(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                //backgroundColor: const Color.fromRGBO(200, 200, 200, 0.8),
+                background: Paint()..color = const Color.fromRGBO(255, 255, 255, 0.6)
+                ..strokeWidth = 35
+                ..strokeJoin = StrokeJoin.round
+                ..strokeCap = StrokeCap.round
+                ..style = PaintingStyle.stroke
+
+                
+                ),
+              ),
+              
+            ),
+      
             Expanded(
               child: Container(
                 child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  
 
                   // Buttons
                   children: [
-                    const Spacer(),
 
-                    // Idealist
-                    ElevatedButton(
-                      onPressed: () {
+                    // Analyst
+                    IconButton(
+                      icon: Image.asset('images/idealist_button.png'),
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(maxHeight: 75, maxWidth: 300),
+                      iconSize: 500,
+                      onPressed: (){
                         setState(() {
                           currentResultImage = getResult(0);
                         }); 
-                      },
-                      child: const Text('Idealist'),
-                    ),
-                    const Spacer(),
 
+                      },
+                    ),
+                    
                     // Synthesist
-                    ElevatedButton(
-                      onPressed: () {
+                    IconButton(
+                      icon: Image.asset('images/synthesist_button.png'),
+                     padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(maxHeight: 75, maxWidth: 300),
+                      iconSize: 500,
+                      onPressed: (){
                         setState(() {
                           currentResultImage = getResult(1);
-                        });
+                        }); 
+
                       },
-                      child: const Text('Synthesist'),
                     ),
-                    const Spacer(),
 
                     // Pragmatist
-                    ElevatedButton(
-                      onPressed: () {
+                    IconButton(
+                      icon: Image.asset('images/prag_button.png'),
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(maxHeight: 75, maxWidth: 300),
+                      iconSize: 500,
+                      onPressed: (){
                         setState(() {
                           currentResultImage = getResult(2);
-                        });
+                        }); 
+
                       },
-                      child: const Text('Pragmatist'),
                     ),
-                    const Spacer(),
 
                     // Analyst
-                    ElevatedButton(
-                      onPressed: () {
+                    IconButton(
+                      icon: Image.asset('images/analyst_button.png'),
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(maxHeight: 75, maxWidth: 300),
+                      iconSize: 500,
+                      onPressed: (){
                         setState(() {
                           currentResultImage = getResult(3);
-                        });
-                        
+                        }); 
+
                       },
-                      child: const Text('Analyst'),
                     ),
-                    const Spacer(),
 
                     // Realist
-                    ElevatedButton(
-                      onPressed: () {
+                    IconButton(
+                      icon: Image.asset('images/realist_button.png'),
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(maxHeight: 75, maxWidth: 300),
+                      iconSize: 500,
+                      onPressed: (){
                         setState(() {
                           currentResultImage = getResult(4);
-                        });
+                        }); 
+
                       },
-                      child: const Text('Realist'),
                     ),
-                    const Spacer(),
                   ],
                 ),
               ),
@@ -91,15 +135,8 @@ class ResultsPageState extends State<ResultsPage> {
 
             // Results
             Expanded(
-              // child: Stack(children: <Widget>[
-              //   Image.asset('assets/images/notepad.png'),
-              //   Center(
-              //       child: Container(
-              //           padding: const EdgeInsets.all(75.0), child: Text(text))),
-              // ]),
-
               child: Container(
-                constraints: BoxConstraints(
+                constraints: const BoxConstraints(
                     maxHeight: 500,
                     maxWidth: 500,
                     minWidth: 500,
@@ -109,20 +146,6 @@ class ResultsPageState extends State<ResultsPage> {
                     image: DecorationImage(
                   image: AssetImage(currentResultImage),
                 )),
-                // padding: const EdgeInsets.all(10.0),
-                // child: Container(
-                //   // constraints: BoxConstraints(maxHeight: 100, maxWidth: 100),
-                //   color: Color.fromARGB(255, 228, 110, 110).withOpacity(0.9),
-                //   // margin: const EdgeInsets.all(150.0),
-
-                //   // constraints: BoxConstraints(maxWidth: 200, maxHeight: 500),
-                //   // padding: const EdgeInsets.all(75.0),
-                //   // alignment: Alignment.center,
-                //   // child: Text(
-                //   //   text,
-                //   //   // textAlign: TextAlign.center,
-                //   // ),
-                // ),
               ),
             ),
           ],
